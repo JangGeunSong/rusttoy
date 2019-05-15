69d3bdd364b47df6fadef8a7dbdd240ad11f342d
JangGeunSong <sjg1468@gmail.com>
2019-05-15 21:02:35 +0900

    add the basic syntax

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 // mod print;
 // mod vars;
 // mod types;
-mod string;
+// mod string;
+// mod tuple;
+// mod array;
+// mod vector;
+// mod conditional;
+mod loops;
 
 fn main() {
     // print::run();
     // vars::run();
     // types::run();
-    string::run();
+    // string::run();
+    // tuple::run();
+    // array::run();
+    // vector::run();
+    // conditional::run();
+    loops::run();
 }
